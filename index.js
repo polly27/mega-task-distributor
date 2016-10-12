058757cbda0a849313627c7d6009721aca120f9f
polly27 <poldro@mail.ru>
2016-10-12 11:01:19 +0300

    show db

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+var pg = require('pg');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	var connectionString = "postgres://fxgvaicejocbqu:dntJNb6sMiK64Pc02lqXnro-PE@" +
+	 "ec2-54-75-226-171.eu-west-1.compute.amazonaws.com:5432:/demja59qg5ck8s";
+
+	pg.connect(connectionString, function(err, client, done) {
+	   client.query('SELECT * FROM Client', function(err, result) {
+	      done();
+	      if(err) return console.error(err);
+	      console.log(result.rows);
+	   });
+	});
+  	response.send(process.env.DATABASE_URL);
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  	console.log("Listening on " + port);
 });
+
