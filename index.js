ddc6a3ccbd1d307abd10d2c7386063c7a463a09c
polly27 <poldro@mail.ru>
2016-10-12 14:34:27 +0300

    connect db 7

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-const pg = require('pg');
+var pg = require('pg');
 
-const connectionString = "postgres://fxgvaicejocbqu:dntJNb6sMiK64Pc02lqXnro-PE@" +
-	 "ec2-54-75-226-171.eu-west-1.compute.amazonaws.com:5432/demja59qg5ck8s";
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.get('/', function(request, response) {
-	const client = new pg.Client(connectionString);
-	client.connect();
 
-	pg.connect(connectionString, function(err, client, done) {
-	   response.send(client);
-	   client.query('SELECT * FROM Contact', function(err, result) {
-	      done();
-	      if(err) return console.error(err);
-	      console.log(result.rows);
-	      response.send(result.rows);
-	   });
-	});
-
+	response.send('success');
+	
 });
 
 var port = process.env.PORT || 5000;
