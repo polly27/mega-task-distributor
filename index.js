453d735bbb989e3d387bb53f0a5fa46fb7ab8c31
polly27 <poldro@mail.ru>
2016-10-27 12:34:13 +0300

    view Shop products 16

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ pg.defaults.ssl = true;
 
 app.get('/', function(request, response) {
 	response.sendfile('index.html');
+});
+
+app.get('/getProducts', function(request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		if (err) throw err;
-		console.log('Connected to postgres! Getting schemas...');
-
 	    var query = client.query("select Name, Name__c, Description__c, Amount__c, Cost__c from salesforce.Product__c;");
 	    query.on("row", function (row, result) { 
 	            result.addRow(row); 
@@ -19,10 +20,9 @@ app.get('/', function(request, response) {
 	    query.on("end", function (result) {          
 	            response.send(result.rows);
 	        });
-
-	    response.sendfile('index.html');
 	});
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
