1f106448be8aa5ae52ec8af20b3fc66e342fe9a4
polly27 <poldro@mail.ru>
2016-10-27 13:02:48 +0300

    view Shop products sum 6

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.get('/', function(request, response) {
 app.get('/getProducts', function(request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		if (err) throw err;
-		var query = client.query("select Id, Name, Name__c, Description__c, Amount__c, Cost__c from salesforce.Product__c;");
+		var query = client.query("select * from salesforce.Product__c");
 		var sum = 0;
    	    query.on("row", function (row, result) { 
-	            sum += row.Cost__c * row.Amount__c; 
+	            result.addRow(row);
 	        });
-		response.send(sum.toString());
+   	    response.send(result.rows[0].Name__c);
 	});
 });
 
