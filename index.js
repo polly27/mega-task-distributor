c4374db812fd4d745deefa0f60520b7aa04285b9
polly27 <poldro@mail.ru>
2016-10-28 09:19:20 +0300

    view catalog and total 5

--- index.js
+++ index.js
@@ -24,20 +24,7 @@ app.get('/getCatalog', function(request, response) {
 	});
 });
 
-app.get('/getTotal', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		if (err) throw err;
-		var query = client.query("select Amount__c, Cost__c from salesforce.Product__c");
-		var total = 0;
-		query.on("row", function (row, result) { 
-   	 	    total += row.amount__c * row.cost__c;
-	    });
-   	    query.on("end", function (result) {
-   	    	console.log('total ' + total);
-   	    	response.send(total);
-	   	});
-	});
-});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
