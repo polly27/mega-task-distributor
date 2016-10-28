b19f3030325eacb890f67a2c44981adb34258efa
polly27 <poldro@mail.ru>
2016-10-28 10:24:41 +0300

    view catalog and total 9

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require("express");
-var app = express();
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
+
 app.use(express.logger());
+app.use(bodyParser.json());
 
 var pg = require('pg');
-
 pg.defaults.ssl = true;
 
 app.get('/', function(request, response) {
@@ -14,25 +17,13 @@ app.get('/getCatalog', function(request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		if (err) throw err;
 		var query = client.query("select Id, Name, Name__c, Description__c, Amount__c, Cost__c from salesforce.Product__c");
-		query.on("row", function (row, result) { 
-   	  	    result.addRow(row);
-   	    });
-   	    query.on("end", function (result) {
-   	    	response.send(JSON.stringify(result.rows));
-	   	});
-	});
-});
-
-app.get('/getTotal', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		if (err) throw err;
-		var query = client.query("select Amount__c, Cost__c from salesforce.Product__c");
 		var total = 0;
 		query.on("row", function (row, result) { 
-   	 	    total += row.amount__c * row.cost__c;
-	    });
+   	  	    result.addRow(row);
+  	  	    total += row.amount__c * row.cost__c;
+   	    });
    	    query.on("end", function (result) {
-   	    	response.send(JSON.stringify(total));
+   	    	response.send({catalog : result.rows, total : total});
 	   	});
 	});
 });
