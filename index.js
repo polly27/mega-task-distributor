b64442ebc2c5e86d0898b85966d9c6ce399e3d60
polly27 <poldro@mail.ru>
2016-10-28 09:06:33 +0300

    view catalog and total 1

--- index.js
+++ index.js
@@ -14,21 +14,28 @@ app.get('/getProducts', function(request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		if (err) throw err;
 		var query = client.query("select Id, Name, Name__c, Description__c, Amount__c, Cost__c from salesforce.Product__c");
-		var total = 0;
 		query.on("row", function (row, result) { 
    	    	    result.addRow(row);
-   	    	    total += row.amount__c * row.cost__c;
-	        });
+   	        });
    	    query.on("end", function (result) {
-   	    	var map = new Map();
-   	    	map.set("catalog", JSON.stringify(result.rows));
-   	    	map.set("total", total);
-   	    	console.log(map);
-   	    	response.send(JSON.stringify(map));
+   	    	response.send(JSON.stringify(result));
 	   	});
 	});
 });
 
+app.get('/getTotal', function(request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) throw err;
+		var query = client.query("select Amount__c, Cost__c from salesforce.Product__c");
+		var total = 0;
+		query.on("row", function (row, result) { 
+   	    	    total += row.amount__c * row.cost__c;
+	        });
+   	    query.on("end", function (result) {
+   	    	response.send(total);
+	   	});
+	});
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
